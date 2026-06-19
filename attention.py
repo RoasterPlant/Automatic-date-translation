da72ccc457e2a14871aa5174bf482b9d576cfd78
RoasterPlant <paulo.pinho.campos@protonmail.com>
2026-06-19 10:52:22 -0300

    Unfinished

--- attention.py
+++ attention.py
@@ -1,4 +1,4 @@
-# Efficient implementation equivalent to the following with bidirectional=False
+
 import os
 import torch
 from torch import nn
@@ -33,14 +33,10 @@ class AttentionLayer(nn.Module):
     def __init__(self, input_size, output_size, activation):
         super().__init__()
         linear = nn.Linear(input_size, output_size)
-        if activation != "cubic":
-            nn.init.kaiming_normal_(linear.weight, mode="fan_in", nonlinearity=activation)
-        else:
-            nn.init.xavier_uniform_(linear.weight, gain=0.5)
         self.stack = nn.Sequential(
             linear,
             activations[activation](),
-            nn.Softmax()
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -48,50 +44,34 @@ class AttentionLayer(nn.Module):
         return logits
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, device, dropout_p=0.1):
+    def __init__(self, input_size, hidden_size, dropout_p=0.1):
         super().__init__()
         self.embedding = nn.Embedding(input_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, bidirectional=True, device=device)
-        #self.attention = AttentionLayer(hidden_size, hidden_size, activation="relu")
+        self.lstm = nn.LSTM(hidden_size, hidden_size, bidirectional=True)
         self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, input):
         embedded = self.dropout(self.embedding(input))
-        output = self.lstm(embedded)
+        output, = self.lstm(embedded)
         return output
 
 class DecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
+        super().__init__()
+        self.hs = hidden_size
         self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.attention = AttentionLayer(hidden_size, hidden_size, activation="relu")
+        self.lstm = nn.LSTMCell(hidden_size, hidden_size, batch_first=True)
         self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
-        batch_size = encoder_outputs.size(0)
-        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=device).fill_(SOS_token)
-        decoder_hidden = encoder_hidden
-        decoder_outputs = []
-
-        for i in range(MAX_LENGTH):
-            decoder_output, decoder_hidden  = self.forward_step(decoder_input, decoder_hidden)
-            decoder_outputs.append(decoder_output)
-
-            if target_tensor is not None:
-                # Teacher forcing: Feed the target as the next input
-                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
-            else:
-                # Without teacher forcing: use its own predictions as the next input
-                _, topi = decoder_output.topk(1)
-                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
-
-        decoder_outputs = torch.cat(decoder_outputs, dim=1)
-        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
-        return decoder_outputs, decoder_hidden, None # We return `None` for consistency in the training loop
-
-    def forward_step(self, input, hidden):
-        output = self.embedding(input)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.out(output)
-        return output, hidden
+    def forward(self, encoder_outputs):
+        attention_output = self.attention(encoder_outputs)
+        hx = torch.zeros(1, self.hs)
+        cx = torch.zeros(1, self.hs)
+        output = []
+        for i in range(attention_output.size()[0]):
+            hx, cx = self.lstm(attention_output[i], (hx, cx))
+            output.append(hx)
+        output = torch.stack(output, dim=0)
+        print(input, "\n-----\n", output)
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import pickle
+import torch
+from torch import nn
 from attention import *
 
 def load_data(filename):
@@ -17,3 +19,21 @@ def load_dataset(filename):
         input_lang.addWord(d[0])
         output_lang.addWord(d[0])
     return input_lang, output_lang, data
+
+#rnn = nn.LSTMCell(10, 20)  # (input_size, hidden_size)
+#input = torch.randn(2, 3, 10)  # (time_steps, batch, input_size)
+#hx = torch.randn(3, 20)  # (batch, hidden_size)
+#cx = torch.randn(3, 20)
+#output = []
+#for i in range(input.size()[0]):
+#    hx, cx = rnn(input[i], (hx, cx))
+#    output.append(hx)
+#output = torch.stack(output, dim=0)
+#print(input, "\n-----\n", output)
+
+rnn = nn.LSTM(10, 10, bidirectional=True)
+input = torch.randn(3, 10)
+output, (hn, cn) = rnn(input)
+softmax = nn.Softmax(dim=1)
+output = softmax(output)
+print(output)
