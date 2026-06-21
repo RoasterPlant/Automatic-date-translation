851007f8254efe2319b823ac48333f4a0820a863
Roaster Plant <paulo.pinho.campos@protonmail.com>
2026-06-21 18:53:13 -0300

    update attention model

--- attention.py
+++ attention.py
@@ -3,27 +3,6 @@ import os
 import torch
 from torch import nn
 
-class Lang:
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {}
-        self.n_words = 0
-
-    def addSentence(self, sentence):
-        for word in sentence.split(' '):
-            self.addWord(word)
-
-    def addWord(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
-        else:
-            self.word2count[word] += 1
-
 activations = {
     "relu": nn.ReLU,
     "tanh": nn.Tanh,
@@ -33,11 +12,13 @@ activations = {
 class AttentionLayer(nn.Module):
     def __init__(self, input_size, output_size, activation):
         super().__init__()
-        linear = nn.Linear(input_size, output_size)
+        linear1 = nn.Linear(input_size, input_size)
+        linear2 = nn.Linear(input_size, output_size)
         self.stack = nn.Sequential(
-            linear,
+            linear1,
             activations[activation](),
-            nn.Softmax(dim=0)
+            linear2,
+            nn.Softmax(dim=1)
         )
 
     def forward(self, x):
@@ -45,22 +26,22 @@ class AttentionLayer(nn.Module):
         return logits
 
 class EncoderRNN(nn.Module):
-    # input_size = Número de componentes de x num estado de tempo. Nesse caso, é o tamanho da representação dos caracteres de entrada (38).
-    # hidden_size = Número de componentes da saída num estado tempo. Ness 
+    # input_size = Número de componentes de x num estado de tempo. Nesse caso, é o tamanho da representação dos caracteres de entrada (37).
+    # hidden_size = Número de componentes da saída num estado tempo.
 
-    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+    def __init__(self, input_size, hidden_size, dropout_p = 0.01):
         super().__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, bidirectional=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, bidirectional=True, batch_first=True)
         self.dropout = nn.Dropout(dropout_p)
 
     def forward(self, input):
-        drop = self.dropout(input)
-        output, = self.lstm(drop)
+        droped = self.dropout(input.to(torch.float32))
+        output, hx = self.lstm(droped)
         return output
 
 class DecoderRNN(nn.Module):
     # hidden_size = O dobro do número de componentes da saída do encoder num estado tempo.
-    # hidden_size = Número de componentes da saída do decoder num estado tempo. Nesse caso, é o tamanho da representação dos caracteres de entrada (11).
+    # output_size = Número de componentes da saída do decoder num estado tempo. Nesse caso, é o tamanho da representação dos caracteres de entrada (11).
 
     def __init__(self, hidden_size, output_size):
         super().__init__()
@@ -68,51 +49,54 @@ class DecoderRNN(nn.Module):
         self.os = output_size
         self.T_y = 10 # Tamanho da data padrão.
         self.attention = AttentionLayer(hidden_size + output_size, 1, activation="relu")
-        self.lstm = nn.LSTMCell(hidden_size, output_size, batch_first=True)
-        self.softmax = nn.Softmax(dim=1)
+        self.lstm = nn.LSTMCell(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=2)
 
     def forward(self, encoder_outputs):
-        T_x = encoder_outputs.size()[0]
-        hx = torch.zeros(self.os)
-        cx = torch.zeros(self.os)
+        batch_size, T_x = encoder_outputs.size()[:2]
+        hx = torch.zeros(batch_size, self.os)
+        cx = torch.zeros(batch_size, self.os)
         output = []
         for _ in range(self.T_y):
-            attention_input = torch.cat([hx.repeat(T_x, 1), encoder_outputs], dim=1)
+            attention_input = torch.cat([hx.repeat(T_x, 1, 1).transpose(0, 1), encoder_outputs], dim=2)
             attention_output = self.attention(attention_input)
-            context = torch.sum(torch.mul(encoder_outputs, attention_output), dim=0)
+            context = torch.sum(torch.mul(encoder_outputs, attention_output), dim=1)
             hx, cx = self.lstm(context, (hx, cx))
             output.append(hx)
-        output = self.softmax(torch.stack(output, dim=0))
+        output = self.softmax(torch.stack(output, dim=1))
         return output
     
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
-        self.layer_stack = nn.Sequential(
-            EncoderRNN(input_size, hidden_size),
-            DecoderRNN(2 * hidden_size, output_size)
-        )
+        self.encoder = EncoderRNN(input_size, hidden_size)
+        self.decoder = DecoderRNN(2 * hidden_size, output_size)
 
     def forward(self, x):
-        logits = self.layer_stack(x)
+        x = self.encoder(x)
+        logits = self.decoder(x)
         return logits
     
-def train(dataset, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimizer):
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
     model.train()
-    for data in dataset:
-        x, y = data
-        
-        # Compute prediction error
-        pred = model(x)
-        loss = loss_fn(pred, y) 
+    total_loss = 0
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred.view(-1, pred.size(-1)), y.view(-1))
 
         # Backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
+        total_loss += loss.item()
+        #for name, param in model.encoder.lstm.named_parameters():
+        #    print(f"Layer Parameter: {name}")
+        #    print(f"Parameter values:  {param}")
 
-    loss = loss.item()
-    print(f"loss: {loss:>7f}")
+    print(f"loss: {total_loss:>7f}")
 
 def test(dataset, model, loss_fn):
     # Unnecessary in this situation but added for best practices
